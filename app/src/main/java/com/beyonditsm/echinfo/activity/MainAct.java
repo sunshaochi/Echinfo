996efdb71f613c0bb026ad05cb78814ef7c5181d
孙少池 <673132032@qq.com>
2016-04-06 09:44:51 +0800

    对外投资 分支机构 纠错界面

--- app/src/main/java/com/beyonditsm/echinfo/activity/MainAct.java
+++ app/src/main/java/com/beyonditsm/echinfo/activity/MainAct.java
@@ -1,20 +1,46 @@
 package com.beyonditsm.echinfo.activity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.beyonditsm.echinfo.R;
+import com.beyonditsm.echinfo.base.BaseActivity;
+import com.lidroid.xutils.view.annotation.event.OnClick;
 
 
 /**
  * 主页面
  */
-public class MainAct extends AppCompatActivity {
+public class MainAct extends BaseActivity {
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void setLayout() {
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void init(Bundle savedInstanceState) {
+
+    }
+
+    @OnClick({R.id.rl_gz,R.id.rl_qy,R.id.rl_sx})
+    public void Onclick(View v) {
+        switch(v.getId()){
+            case R.id.rl_gz:
+                openActivity(MyFollowAct.class);
+               break;
+            case R.id.rl_qy:
+
+                break;
+
+           }
+
+
+
     }
 
 
